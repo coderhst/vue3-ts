c944a6da7f715b0bdde19382fddfb78375276020
coderhst <1727926855@qq.com>
2022-06-21 21:57:52 +0800

    vue-master

---
+++ src/demo.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="content">
+    <h3 @click="handleDemoClick">Hello</h3>
+    <h4 @click="handleClickDemo">name</h4>
+    <h4 @click="handleQuickClick"></h4>
+    <h5></h5>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: '',
+  props: {},
+  components: {},
+  setup() {
+    const name = ref('coderWhy')
+
+    const handleClickDemo = () => {
+      console.log('Hello Word')
+    }
+
+    const handleDemoClick = () => {
+      console.log('12342342')
+    }
+
+    const handleQuickClick = () => {
+      console.log('Demo Vue')
+    }
+    return {
+      name,
+      handleClickDemo,
+      handleDemoClick,
+      handleQuickClick
+    }
+  }
+})
+</script>
+
+<style scoped lang="less"></style>
